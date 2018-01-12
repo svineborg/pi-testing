f7d43f1d8e50b78042140b783bcfa6e77817aefd
Jared <svineborg@gmail.com>
2018-01-11 23:13:26 -0800

    added codepen work

--- game.js
+++ game.js
@@ -1,6 +1,12 @@
-$('document').ready(function(){
-	$('.btn').click(function(){
-		console.log('okay');
-		$('p').toggle(1000);
-	});
+$(document).ready(function() {
+  var choices = function(num_of_choices){
+    console.log('num_of_choices:' + num_of_choices);
+    for (var i = 0; i < 4; i++){
+      $('div.btm').append('<div class="added">' + i + '<button class="btn">another</button></div>');
+    }
+  };
+  $(".btn").click(function() {
+    $(".btn").text('begin the game');
+    choices(3);
+  });
 });
